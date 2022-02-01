1b01a468a758e143e4ccff0295496c9d7661c2ec
nader019 <gw8850@wayne.edu>
2022-01-31 22:48:27 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Header file for input output functions
+//Author: Nader Rahman <gw8850@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
